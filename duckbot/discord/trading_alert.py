1da5a986716091174257df151b04eb82e34fe11a
Shuri2060 <29679284+Shuri2060@users.noreply.github.com>
2024-09-22 06:35:23 +0800

    discord trade alert

--- duckbot/discord/trading_alert.py
+++ duckbot/discord/trading_alert.py
@@ -1,7 +1,37 @@
+from pathlib import Path
 from .webhook import *
 
 
-webhook = Webhook.from_config_file('configs/config_staging')
-webhook.execute(Message(
-    'test',
+webhook = Webhook.from_config_file(Path(__file__).parent / '../configs/config_staging.json')
+
+
+def trade_alert(
+    market_name: str,
+    taker_address: str,
+    size: float,
+    is_yes: bool,
+    percent: float,
+):
+    return webhook.execute(Message(
+    embeds=[
+        Embed(
+            title='Trade Made',
+            description=f'`{market_name}`',
+            color=0xFFFF00,
+            fields=[
+                Field('User', f'`{taker_address}`', False),
+                Field('Bet Size', f'`{size}`', True),
+                Field('Side', '`YES`' if is_yes else '`NO`', True),
+                Field('Odds', f'`{percent:.2f}% | {1} : {100 / percent - 1:.2f}`', True),
+            ],
+        ),
+    ],
 ))
+
+# trade_alert(
+#     'Donald Trump opens a farm tomorrow',
+#     '0x' + '0' * 40,
+#     21.2,
+#     True,
+#     40.0,
+# )
